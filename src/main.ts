161a2933f6672833d86feeafe74d441dc91ae705
sergeytokarev <stokarev@novakidschool.com>
2024-08-02 23:52:44 +0700

    working assets in public, prettier

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import * as Phaser from 'phaser'
-import {MainScene} from './scenes/MainScene';
+import * as Phaser from "phaser";
+import { MainScene } from "./scenes/MainScene";
 
 const config: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {x: 0, y: 200}
-        }
-    },
-    scene: [MainScene]
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 200 }
+    }
+  },
+  scene: [MainScene]
 };
 
 new Phaser.Game(config);
--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -1,31 +1,31 @@
-import * as Phaser from 'phaser';
+import * as Phaser from "phaser";
 
 export class MainScene extends Phaser.Scene {
-    constructor() {
-        super('MainScene');
-    }
+  constructor() {
+    super("MainScene");
+  }
 
-    preload() {
-        this.load.image('sky', 'assets/images/space3.png');
-        this.load.image('logo', 'assets/images/phaser3-logo.png');
-        this.load.image('red', 'assets/images/red.png')
-    }
+  preload() {
+    this.load.image("sky", "assets/images/space3.png");
+    this.load.image("logo", "assets/images/phaser3-logo.png");
+    this.load.image("red", "assets/images/red.png");
+  }
 
-    create() {
-        this.add.image(400, 300, 'sky');
+  create() {
+    this.add.image(400, 300, "sky");
 
-        const particles = this.add.particles(0, 0, 'red', {
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
+    const particles = this.add.particles(0, 0, "red", {
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: "ADD"
+    });
 
-        const logo = this.physics.add.image(400, 100, 'logo');
+    const logo = this.physics.add.image(400, 100, "logo");
 
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
 
-        particles.startFollow(logo);
-    }
+    particles.startFollow(logo);
+  }
 }
